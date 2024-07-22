65b1755dbb169f7ec1e1815d7679cc14ef75c1dd
AHaydous <amh118@usal.edu.lb>
2024-07-22 16:33:28 +0300

    Initial commit

--- XRayReportApi/obj/Debug/net8.0/XRayReportApi.AssemblyInfo.cs
+++ XRayReportApi/obj/Debug/net8.0/XRayReportApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XRayReportApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4d68deb6c4281a791471513626b19a11e44c354")]
 [assembly: System.Reflection.AssemblyProductAttribute("XRayReportApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XRayReportApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
